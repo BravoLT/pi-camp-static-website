a2374368e769b2e7f3c556327a9508f1a264f507
Justin Wheeler <justin.wheeler@gfs.com>
2024-06-20 21:29:03 -0400

    update tunnel-cloudflared.sh script

--- hosting/tunnel-cloudflared.sh
+++ hosting/tunnel-cloudflared.sh
@@ -2,8 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo cloudflared tunnel --url http://localhost:80 > logs/tunnel.log
-
-sleep 10
-
-grep -P https.*trycloudflare.com logs/tunnel.log
+sudo cloudflared tunnel --url http://localhost:80
