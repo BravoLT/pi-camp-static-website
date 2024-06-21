fd7f9cc81b199bf8c529f594620d98224b4c2a05
Justin Wheeler <justin.wheeler@gfs.com>
2024-06-20 21:17:26 -0400

    make it easier for kids to get url

--- hosting/tunnel-cloudflared.sh
+++ hosting/tunnel-cloudflared.sh
@@ -2,4 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo cloudflared tunnel --url http://localhost:80
+sudo cloudflared tunnel --url http://localhost:80 > logs/tunnel.log
+
+sleep 10
+
+grep -P https.*trycloudflare.com logs/tunnel.log
