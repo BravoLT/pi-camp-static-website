1f8f4b6e2de89a39a774caace511660c884c6733
Justin Wheeler <justin.wheeler@gfs.com>
2024-06-21 14:12:21 -0400

    added logic in install-cloudflared.sh script to flip based on architecture

--- hosting/install-cloudflared.sh
+++ hosting/install-cloudflared.sh
@@ -6,10 +6,33 @@ sudo apt update
 
 sudo apt upgrade -y
 
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb
+sudo dpkg --add-architecture arm
 
-sudo apt install ./cloudflared-linux-arm.deb
+ARCH=$(uname -m)
 
-sudo rm cloudflared-linux-arm.deb
+echo "architecture: ${ARCH}"
 
-sudo cloudflared tunnel --url http://localhost:8080
+# install 32-bit package
+if [ ${ARCH} -eq 'armv7l' ]; then
+
+  wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb
+
+  sudo apt install ./cloudflared-linux-arm.deb
+
+  sudo rm cloudflared-linux-arm.deb
+
+# install 64-bit package
+elif [[ "$ARCH" == 'aarch64' ]]; then
+
+  wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb
+
+  sudo apt install ./cloudflared-linux-arm64.deb
+
+  sudo rm cloudflared-linux-arm64.deb
+
+else
+
+  echo "unknown architecture: ${ARCH}"
+
+  echo "install manually: https://github.com/cloudflare/cloudflared/releases"
+fi
